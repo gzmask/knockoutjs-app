7b272139f8aadd2d3171f0418358f34999b462f0
gzmask <gzmask@gmail.com>
2015-09-14 23:20:50 -0600

    adding calendar table

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-  var vm = { 
-    Markets: states,
-    Routes: routes,
-    Drivers: drivers
+  // each week of the current month will be populated once a month is selected.
+  var Week = 
+    function(days/*fixed length array of 7 booleans indicating the checkbox values, null if not exist*/) { 
+      if (days.length != 7) throw "invalid week";
+      this.days = ko.observableArray(daysofmonth); };
+  var vm = {
+    markets: 
+      states,
+    routes: 
+      routes,
+    drivers: 
+      drivers,
+    weeks: 
+      //nested array representing a month: 5 weeks for a month, 7 days for a week.
+      Array.apply(null, Array(5)).map(function(){return new Week(Array.apply(null, Array(7)).map(function(){return null}))}),
+    Month:
+      9,
+    Year:
+      2015
   };
   ko.applyBindings(vm);
 });
