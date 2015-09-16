01678950ade34a5859011122c24ffefbf53f96dd
gzmask <gzmask@gmail.com>
2015-09-15 23:52:58 -0600

    added month year to view

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var Week = 
     function(days/*fixed length array of 7 booleans indicating the checkbox values, null if not exist*/) { 
       if (days.length != 7) throw "invalid week";
-      this.days = ko.observableArray(daysofmonth); };
+      this.days = ko.observableArray(days); };
   var vm = {
     markets: 
       states,
@@ -13,11 +13,11 @@ $(document).ready(function() {
       drivers,
     weeks: 
       //nested array representing a month: 5 weeks for a month, 7 days for a week.
-      Array.apply(null, Array(5)).map(function(){return new Week(Array.apply(null, Array(7)).map(function(){return null}))}),
-    Month:
-      9,
-    Year:
-      2015
+      Array.apply(null, Array(5)).map(function(){return new Week(Array.apply(null, Array(7)).map(function(){return false}))}),
+    month:
+      ko.observable(9),
+    year:
+      ko.observable(2015)
   };
   ko.applyBindings(vm);
 });
