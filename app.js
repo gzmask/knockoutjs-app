7d8c4998c01393649bb8ad6e6fc3177080c644b4
gzmask <gzmask@gmail.com>
2015-09-17 17:13:20 -0600

    added post button and event

--- app.js
+++ app.js
@@ -8,24 +8,25 @@ var Day = function(display, checked, dayofmonth/*optional*/) {
 // each week of the current month will be populated once a month is selected.
 var Week = function(days/*fixed length array of 7 booleans indicating the checkbox values, null if not exist*/) {
     if (days.length != 7) throw "invalid week";
-    this.days = ko.observableArray(days); };
+    this.days = ko.observableArray(days); };// <<------------- This is the required observable array for the checkboxes.
 
 //changes the current month
 function changeMonth(year, month, weeks/*the weeks of a month in an observalbe array*/){
 
   //initialize the month arrays
   for(var i = 0; i< 7; i++) {
-    weeks()[0].days()[i].display(false);
-    weeks()[0].days()[i].dayofmonth(null);
-    weeks()[1].days()[i].display(false);
-    weeks()[1].days()[i].dayofmonth(null);
-    weeks()[2].days()[i].display(false);
-    weeks()[2].days()[i].dayofmonth(null);
-    weeks()[3].days()[i].display(false);
-    weeks()[3].days()[i].dayofmonth(null);
-    weeks()[4].days()[i].display(false);
-    weeks()[4].days()[i].dayofmonth(null);
-  }
+    weeks[0].days()[i].display(false);
+    weeks[0].days()[i].dayofmonth(null);
+    weeks[1].days()[i].display(false);
+    weeks[1].days()[i].dayofmonth(null);
+    weeks[2].days()[i].display(false);
+    weeks[2].days()[i].dayofmonth(null);
+    weeks[3].days()[i].display(false);
+    weeks[3].days()[i].dayofmonth(null);
+    weeks[4].days()[i].display(false);
+    weeks[4].days()[i].dayofmonth(null);
+    weeks[5].days()[i].display(false);
+    weeks[5].days()[i].dayofmonth(null);}
 
   //get first day of the month and set counters
   var d = new Date(year, month, 1);
@@ -37,16 +38,17 @@ function changeMonth(year, month, weeks/*the weeks of a month in an observalbe a
 
   //filling the dates
   for(var i=1; i<=d.getDate(); i++) {
-    weeks()[weekCounter].days()[dayCounter].display(true);
-    weeks()[weekCounter].days()[dayCounter].dayofmonth(i);
+    weeks[weekCounter].days()[dayCounter].display(true);
+    weeks[weekCounter].days()[dayCounter].dayofmonth(i);
     dayCounter += 1;
     if (dayCounter == 7) 
       weekCounter += 1;
     dayCounter%=7;
   }
 
-  //unessary: weeks()[0].days.valueHasMutated();
+  //unessary: weeks[0].days.valueHasMutated();
 }
+
 
 //The knockout view model class
 var VM = function(){
@@ -55,8 +57,11 @@ var VM = function(){
   self.routes=routes,
   self.drivers=drivers;
 
-  //initialze an nested observable array representing a month: 5 weeks for a month, 7 days for a week.
-  self.weeks=ko.observableArray(Array.apply(null, Array(5)).map(function(){return new Week(Array.apply(null, Array(7)).map(function(){return new Day(false, false)}))}));
+  //initialze an array representing a month: 6 weeks for a month, 7 days for a week.
+  self.weeks=
+      Array.apply(null, Array(6)).map(
+        function(){return new Week(Array.apply(null, Array(7)).map(
+            function(){return new Day(false, false)}))});
 
   //current month
   self.month=ko.observable(9);
@@ -65,46 +70,53 @@ var VM = function(){
   //current month in words
   self.monthWord=
     ko.pureComputed(function(){
-      switch (self.month()) {
-        case 0: return "Jan"
-                break;
-        case 1: return "Feb"
-                break;
-        case 2: return "Mar"
-                break;
-        case 3: return "Apr"
-                break;
-        case 4: return "May"
-                break;
-        case 5: return "Jun"
-                break;
-        case 6: return "Jul"
-                break;
-        case 7: return "Aug"
-                break;
-        case 8: return "Sep"
-                break;
-        case 9: return "Oct"
-                break;
-        case 10: return "Nov"
-                break;
-        case 11: return "Dec"
-                break;}
+      return ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][self.month()];
     });
+
+  //handling the weekly repeat routes
+  self.weekThs = ['Su','Mo','Tu','We','Th','Fr','Sa'];
+  self.weeklyRepeatRoute = function(day/*0 is Sunday, 1 is Monday and so on.*/){
+    day = day || 0;
+    if (self.weeks[0].days()[day].checked() &&
+        self.weeks[1].days()[day].checked() &&
+        self.weeks[2].days()[day].checked() &&
+        self.weeks[3].days()[day].checked())
+    { 
+    for(var i = 0; i<6; i++) 
+      self.weeks[i].days()[day].checked(false);}
+    else {
+    for(var i = 0; i<6; i++) 
+      self.weeks[i].days()[day].checked(true);}
+  };
+
+  //AJAX using Jquery
+  self.ajaxPost = function(){
+    var data = { 
+      market: $("#marketsID option:selected").text(),
+      routes: $("#routesID option:selected").text(),
+      drivers: $("#driversID option:selected").text(),
+      year: self.year(),
+      month: self.monthWord() 
+    };
+    console.log('Posting Data:');
+    console.log(data);
+    $.post( "ajax/test.html", data).done(function( data ) {
+      console.log("Data posted: " + data);});
+  }
 
   //button events for changing current month and year
   self.increaseCurrentMonth=function(){
-      this.month((this.month()+1)%12);
-      changeMonth(this.year(), this.month(), this.weeks);};
+      self.month((self.month()+1)%12);
+      changeMonth(self.year(), self.month(), self.weeks);};
   self.decreaseCurrentMonth=function(){
-      this.month((12+(this.month()-1)%12)%12);
-      changeMonth(this.year(), this.month(), this.weeks);};
+      self.month((12+(self.month()-1)%12)%12);
+      changeMonth(self.year(), self.month(), self.weeks);};
   self.increaseCurrentYear=function(){
-      this.year((this.year()+1)%3000);
-      changeMonth(this.year(), this.month(), this.weeks);};
+      self.year((self.year()+1)%3000);
+      changeMonth(self.year(), self.month(), self.weeks);};
   self.decreaseCurrentYear=function(){
-      this.year((this.year()-1)%3000);
-      changeMonth(this.year(), this.month(), this.weeks);};
+      self.year((self.year()-1)%3000);
+      changeMonth(self.year(), self.month(), self.weeks);};
 };
 
 //main binding on ready
