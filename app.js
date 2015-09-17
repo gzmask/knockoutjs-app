f18d2a812dc3a20a4437cf7d4a47d4b2d1a51dd2
gzmask <gzmask@gmail.com>
2015-09-17 10:44:38 -0600

    days as obervable array is done. next trying weeks as obervable array

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
+
+  //each day of the week will has a bool property of checked, and a bool display property defining if the day of the month is valid.
+  var Day = function(display,checked) {
+    this.display = display;
+    this.checked = checked; };
+
   // each week of the current month will be populated once a month is selected.
-  var Week = 
-    function(days/*fixed length array of 7 booleans indicating the checkbox values, null if not exist*/) { 
+  var Week = function(days/*fixed length array of 7 booleans indicating the checkbox values, null if not exist*/) {
       if (days.length != 7) throw "invalid week";
       this.days = ko.observableArray(days); };
+
   var vm = {
     markets: 
       states,
@@ -13,11 +19,15 @@ $(document).ready(function() {
       drivers,
     weeks: 
       //nested array representing a month: 5 weeks for a month, 7 days for a week.
-      Array.apply(null, Array(5)).map(function(){return new Week(Array.apply(null, Array(7)).map(function(){return false}))}),
+      Array.apply(null, Array(5)).map(function(){return new Week(Array.apply(null, Array(7)).map(function(){return new Day(false, false)}))}),
     month:
       ko.observable(9),
     year:
       ko.observable(2015)
   };
+
   ko.applyBindings(vm);
+  vm.weeks[0].days.replace(vm.weeks[0].days()[0], new Day(true,true));
+  console.log(vm.weeks[0].days());
+  console.log(vm.weeks[0]);
 });
